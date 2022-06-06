dbc5da6ca71d06b312976fed501e3ff516d68379
danyBautista <darkness.bc49@gmail.com>
2022-06-06 12:01:34 -0500

    dasboard

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,6 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from dataclasses import dataclass
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
@@ -29,6 +30,38 @@ class DashboardHome(HttpResponse):
         approved = accreditation.objects.all().filter(status = 'ACREDITADO')
         pending = accreditation.objects.all().filter(status = 'PENDIENTE')
         retired = accreditation.objects.all().filter(status = 'RETIRADO')
+        DataChar = {
+            'C2_A' : accreditation.objects.all().filter(route__concession = 'C2', status='ACREDITADO').count(),
+            'C3_A' : accreditation.objects.all().filter(route__concession = 'C3', status='ACREDITADO').count(),
+            'C4_A' : accreditation.objects.all().filter(route__concession = 'C4', status='ACREDITADO').count(),
+            'C5_A' : accreditation.objects.all().filter(route__concession = 'C5', status='ACREDITADO').count(),
+            'C6_A' : accreditation.objects.all().filter(route__concession = 'C6', status='ACREDITADO').count(),
+            'C7_A' : accreditation.objects.all().filter(route__concession = 'C7', status='ACREDITADO').count(),
+            'C8_A' : accreditation.objects.all().filter(route__concession = 'C8', status='ACREDITADO').count(),
+            'C9_A' : accreditation.objects.all().filter(route__concession = 'C9', status='ACREDITADO').count(),
+            'C10_A' : accreditation.objects.all().filter(route__concession = 'C10', status='ACREDITADO').count(),
+            'C11_A' : accreditation.objects.all().filter(route__concession = 'C11', status='ACREDITADO').count(),
+            'C2_P' : accreditation.objects.all().filter(route__concession = 'C2', status='PENDIENTE').count(),
+            'C3_P' : accreditation.objects.all().filter(route__concession = 'C3', status='PENDIENTE').count(),
+            'C4_P' : accreditation.objects.all().filter(route__concession = 'C4', status='PENDIENTE').count(),
+            'C5_P' : accreditation.objects.all().filter(route__concession = 'C5', status='PENDIENTE').count(),
+            'C6_P' : accreditation.objects.all().filter(route__concession = 'C6', status='PENDIENTE').count(),
+            'C7_P' : accreditation.objects.all().filter(route__concession = 'C7', status='PENDIENTE').count(),
+            'C8_P' : accreditation.objects.all().filter(route__concession = 'C8', status='PENDIENTE').count(),
+            'C9_P' : accreditation.objects.all().filter(route__concession = 'C9', status='PENDIENTE').count(),
+            'C10_P' : accreditation.objects.all().filter(route__concession = 'C10', status='PENDIENTE').count(),
+            'C11_P' : accreditation.objects.all().filter(route__concession = 'C11', status='PENDIENTE').count(),
+            'C2_R' : accreditation.objects.all().filter(route__concession = 'C2', status='RETIRADO').count(),
+            'C3_R' : accreditation.objects.all().filter(route__concession = 'C3', status='RETIRADO').count(),
+            'C4_R' : accreditation.objects.all().filter(route__concession = 'C4', status='RETIRADO').count(),
+            'C5_R' : accreditation.objects.all().filter(route__concession = 'C5', status='RETIRADO').count(),
+            'C6_R' : accreditation.objects.all().filter(route__concession = 'C6', status='RETIRADO').count(),
+            'C7_R' : accreditation.objects.all().filter(route__concession = 'C7', status='RETIRADO').count(),
+            'C8_R' : accreditation.objects.all().filter(route__concession = 'C8', status='RETIRADO').count(),
+            'C9_R' : accreditation.objects.all().filter(route__concession = 'C9', status='RETIRADO').count(),
+            'C10_R' : accreditation.objects.all().filter(route__concession = 'C10', status='RETIRADO').count(),
+            'C11_R' : accreditation.objects.all().filter(route__concession = 'C11', status='RETIRADO').count()
+        }
         context = {
                     'segment': 'index',
                     'sidebars': sidebar,
@@ -40,7 +73,8 @@ class DashboardHome(HttpResponse):
                     'retired' : retired,
                     'vehicles' : vh,
                     'people' : pe,
-                    'business' : bu
+                    'business' : bu,
+                    'data' : DataChar
                 }
 
         html_template = loader.get_template('home/index.html')
