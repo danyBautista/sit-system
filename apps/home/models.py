ba42e54456b73ad4a2bc0c89c73022390907612b
danyBautista <darkness.bc49@gmail.com>
2022-06-23 21:06:45 -0500

    layout consulting

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,6 +8,7 @@ from core.user.models import User
 # Create your models here.
 
 class Consulting(models.Model):
+    id = models.AutoField(primary_key=True)
     key_register = models.CharField(max_length=15, null=True, blank=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     date_query = models.DateTimeField(auto_now_add=True)
